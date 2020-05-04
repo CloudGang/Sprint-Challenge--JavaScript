2b45e1232f957323e4b59bf5048be2132b301fcf
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-03 22:14:39 -0400

    Update functions.js

--- challenges/functions.js
+++ challenges/functions.js
@@ -15,6 +15,17 @@ function consume (a, b, cb) {
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
+function add(a, b) {
+  return a + b;
+};
+
+function multiply(a, b) {
+  return a * b;
+};
+
+function greeting(first_name, last_name) {
+  return `Hello ${first_name} ${last_name}, nice to meet you!`
+}
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
