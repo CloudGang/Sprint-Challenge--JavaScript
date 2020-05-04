171d825766f25da3f9de7d482e7bb527beedc469
cloudgang <35246400+CloudGang@users.noreply.github.com>
2020-05-03 22:06:39 -0400

    Update objects-arrays.js

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -18,9 +18,21 @@ const trex = {
   }
 }
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
-
+const stegos = {
+  name: 'stegosaurus',
+  diet: 'herbivorous',
+  weight: '2000kg',
+  length: '9m',
+  period: 'Late Jurassic'
+}
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const seizer = {
+  name: 'velociraptor',
+  diet: 'carnivorous',
+  weight: '15kg',
+  length: '1.8m',
+  period: 'Late Cretaceous'
+}
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
